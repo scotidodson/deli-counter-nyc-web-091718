6caaf503f2e0cc9b8f9a3c22fd0310c5a1871929
Scoti Dodson <scotidodson@gmail.com>
2018-08-27 20:09:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
-    katz_deli.length = end
+    katz_deli_line = []
     katz_deli.each_with_index do |name, index|
       number = index + 1 
       katz_deli_line << "#{number}. #{name} "
