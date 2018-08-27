b7d8fa2215a5b62c5be4bd373bcc72c3ad4c9306
Scoti Dodson <scotidodson@gmail.com>
2018-08-27 19:24:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,10 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
+  else 
+    puts "The line is currently: " 
   end 
 end 
+
+def take_a_number(katz_deli, name)
+  katz_deli << 
